64223f6475a9d8636109070c2bb639161a0601cc
akiselevskis <arturs.kiselevskis@gmail.com>
2023-10-03 11:51:19 +0300

    Class_05

---
+++ Class_07/ezis.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main(){
+  printf("Ezis iet \tpa taci\rņu pa \b\bmazu \nmazu taciņu");
+  printf("Kaba\atā nes maciņu");
+  printf("Ole, \vole, ole!\n");
+  return 0;
+  /* outputs:
+ņu pmazu 	pa taci
+mazu taciņuKabatā nes maciņuOle, 
+                                 ole, ole!
+  */
+  }
