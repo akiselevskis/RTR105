2150805e87ba997adbfca96f3cb1f4019fc2fc67
akiselevskis <arturs.kiselevskis@gmail.com>
2023-10-15 18:52:51 +0300

    Class_05

---
+++ Majasdarbi/PU1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    long int input1, input2, returnValue;
+    printf("Lūdzu ievadīt abus skaitļus tā, lai starp cipariem nebūtu atstarpes\n"); 
+    printf("un lai starp skaitļiem būtu atstarpe vai \"Enter\":");
+    scanf("%ld", &input1); //Pirmais mainīgais = 400000
+    scanf("%ld", &input2); //Otrais mainīgais = 100000
+    returnValue = input1*input2; //Mainīgo reizinājums = 100000*400000 = 40000000000
+    printf("%ld\n", returnValue); //Izdod reizinājumu
+    return 0;
+}
+        //Šī programma nestrādā, ja starp cipariem ievieto atstarpes (piem. 400 000).
+        //Lai tā strādātu, jaaievieto 400000 un 100000.
+        //Lūdzu dodiet ziņu, ja šī problēma ir jālabo. Paldies!
+
