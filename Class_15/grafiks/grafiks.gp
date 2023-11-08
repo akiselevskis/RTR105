cc1b9e256f5a80dbc80baaed8152bcd57dcc219e
akiselevskis <arturs.kiselevskis@gmail.com>
2023-11-08 18:29:10 +0200

    Class_05

--- Class_15/grafiks/grafiks.gp
+++ Class_15/grafiks/grafiks.gp
@@ -12,12 +12,17 @@ set mytics 5
 set grid mxtics mytics
 #sets minor grid with 5 increments 
 
-pointtype 7
+set y2tics -100, 10
+set ytics nomirror
+set yrange [-2:2]
+set y2range [-0.5:0.5]
+
+#pointtype 7
 
 
-#plot [-2*pi:2*pi] sin(x) with linespoints pt 7
-
-plot [-2*pi:2*pi] sin(x
+plot [-2*pi:2*pi] sin(x) with linespoints pt 7
+#f(x) = sin(x)
+#plot [-2*pi:2*pi] f(x)
 
 #set size 0.5, 0.5
 #set term png size 600, 400
--- Class_15/teilors5.c
+++ Class_15/teilors5.c
@@ -2,22 +2,23 @@
 #include<math.h>
 void main(){
  double x=2.05,y,a,S;
+ int k=0;
  y = sin(x);
  printf("y=sin(%.2f)=%.2f\n", x, y);
 
- a = pow(-1,0)*pow(x,2*0+1)/(1.);
+ a = pow(-1,k)*pow(x,2*0+1)/(1.);
  S = a;
  printf("%.2f\t%8.2f\t%8.2f\n", x, a, S);
 
- a = a* (-1) *x*x / (2*3);
+ a = a* (-1) *x*x / ((2*k)*(2*k+1));
  S = S + a;
  printf("%.2f\t%8.2f\t%8.2f\n", x, a, S);
-
- a = a* (-1)  *x*x / (4*5);
+ k++;
+ a = a* (-1)  *x*x / ((2*k)*(2*k+1));
  S = S + a;
  printf("%.2f\t%8.2f\t%8.2f\n", x, a, S);
-
- a = a* (-1)  *x*x / (6*7);
+ k++;
+ a = a* (-1)  *x*x / ((2*k)*(2*k+1));
  S = S + a;
  printf("%.2f\t%8.2f\t%8.2f\n", x, a, S);
 }
