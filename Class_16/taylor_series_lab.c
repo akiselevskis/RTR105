be0b60f5c7947147ecacff1bd2b113e80da03f4a
akiselevskis <78474345+akiselevskis@users.noreply.github.com>
2023-11-21 08:14:22 +0200

    Create taylor_series_lab.c

---
+++ Class_16/taylor_series_lab.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.>
+
+int main()
+{
+    printf("Funckijas - cos(x/2)^2 aprekinasana:");
+    printf("Ievadiet argumentu x: ");
+    scanf("%fl", x);
+    int S = 
+
+    return 0;
+}
