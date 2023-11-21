53914bf7f22b8e1e5abd2a36ede9e85eafe39b72
akiselevskis <78474345+akiselevskis@users.noreply.github.com>
2023-11-21 08:11:06 +0200

    Update Taylor_series_test.c

--- Class_16/Taylor_series_test.c
+++ Class_16/Taylor_series_test.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<math.h>
-double mans_sinuss(double x){
+double mana_funckija(double x){
  double a,S;
  int k=0;
 
  //a = pow(-1,k)*pow(x,2*k+1)/(1.);
- a = pow(-1, k)*pow(x, 2*k)/
+ a = pow(-1, k)*pow(x, 2*k); //rekurences reizinataja formula
  //S = a;
- S = (1/2 + 1/2)*a;
+ S = (1/2 + 1/2)*a; //funkcijas vertiba
  printf("%.2f\t%8.2f\t%8.2f\n", x, a, S);
 
  while(k<3){
@@ -25,6 +25,6 @@ void main(){
  y = cos(x/2)*cos(x/2);
  printf("standarta funkcija sin - y=sin(%.2f)=%.2f\n", x, y);
 
- yy = mans_sinuss(x);
+ yy = mana_funckija(x);
  printf("lietotaaja funkcija - y=mans_sinuss(%.2f)=%.2f\n", x, yy);
 }
