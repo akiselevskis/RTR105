942c5217ae3ee5e21c7110abfd62f9c367aa6759
= <=>
2023-11-22 18:51:05 +0200

    22_11_23

--- Class_16/taylor.c
+++ Class_16/taylor.c
@@ -1,28 +1,42 @@
 #include<stdio.h>
 #include<math.h>
-double teilors(double x){
- double a,S,Sg; //Sg ir S gala
+void asci();
+double mans_sinuss(double x){
+ double a,S,Sg;
  int k=0;
 
- a = pow(-1, k)*pow(x, 2*k); //pirmā locekla a formula
- S = a; //summas dalas pirmais loceklis (kad k = 0)
- Sg = 1/2 + 1/2*a; //funkcijas vertiba
- printf("%.2f\t%8.2f\t%8.2f\n", x, a, Sg);
+ a = 1; //calculate the first element of the sum
+ S = a; //push it to the sums value
+ printf("%.2f\t%8.2f\t%8.2f\n", x, a, S); //printf those
 
- while(k<10){
-  k++;
-  a = a * (-1) *x*x / (4*k*k - 2*k); //loceklis a reiz rekurences reizinatajs kur mainās k
-  S = S + a;
-  printf("%.2f\t%8.2f\t%8.2f\n", x, a, S);
+ while(k<500){ //4 loops
+  k++; //k = 1
+  a = a* ((-1) *x*x) / (4*k*k - 2*k); //rekurences reizinatajs ar ieprieksejo a vertibu
+  S = S + a; //jaunais sum (pieskaitot jauno a)
+  Sg = 0.5 + 0.5*S;
+  printf("%.2f\t%8.2f\t%8.2f\n", x, a, Sg); //print those
+  void asci();
  }
-  return 1/2 + 1/2*S;
+  return Sg;
 }
 
-void main(){
- double x = 5, y, yy;
+void main(){/*
+ double x = 0.9, y, yy;
  y = cos(x/2)*cos(x/2);
- printf("math library funkcija cos(x/2)*cos(x/2) - y=cos(%.2f/2)*cos(%.2f/2)=%.2f\n", x, x, y);
+ printf("standarta funkcija sin - y=sin(%.2f)=%.2f\n", x, y);
 
- yy =teilors(x);
- printf("taylor funkcijas vertiba - y=mans_sinuss(%.2f)=%.2f\n", x, yy);
+ yy = mans_sinuss(x);
+ printf("lietotaaja funkcija - y=mans_sinuss(%.2f)=%.2f\n", x, yy);*/
+ asci();
+}void asci(){
+    printf("                                       500                      \n");
+    printf("                                     _______                    \n");
+    printf("                                    \\                          \n");
+    printf("                                     \\          k   2*k        \n");
+    printf("                      1     1         \\     (-1) * x           \n");
+    printf("   cos(x\2)*cos(x/2) = ___ + ___ *       > __________________   \n");
+    printf("                                      /                         \n");
+    printf("                      2     2        /       (2*k)\!;           \n");
+    printf("                                    /_______                    \n");
+    printf("                                       k=0                      \n\n");
 }
