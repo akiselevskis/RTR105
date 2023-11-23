6c786a06a91172534581e861f0179cd8c16d157a
= <=>
2023-11-23 13:28:47 +0200

    23_11_23

--- Class_16/taylor.c
+++ Class_16/taylor.c
@@ -1,46 +1,55 @@
 #include<stdio.h>
 #include<math.h>
-void asci();
-double mans_sinuss(double x){
- double a,S,Sg;
- int k=0;
+void ascii(); //funckiju ar ASCII zimejumu deklare atseviski lai ta netraucetu koda uztverei
+double teilors(double x){
+ double a,S,Sg; //a ir pirmais elements, S ir summa, kas mainas pec katras cilpas atkartosanas reizes 
+ int k=0;       //Sg ir gala funckijas rezultats
 
- a = 1; //calculate the first element of the sum
- S = a; //push it to the sums value
- printf("%.2f\t%8.2f\t%8.2f\n", x, a, S); //printf those
+ a = 1; //pirmais funckijas elements ir 1 (aprekinats atseviski)
+ S = a; //S ir vertibas uztures mainigais (pie ta pieskaitis katru elementu)
+ Sg = 0.5 + 0.5*S; //vert
+ printf("Pirmais summas elements a0 = S0 = %.2f\n", a, Sg);
 
- while(k<500){ //4 loops
+ while(k<500){
   k++; //k = 1
   a = a* ((-1) *x*x) / (4*k*k - 2*k); //rekurences reizinatajs ar ieprieksejo a vertibu
   S = S + a; //jaunais sum (pieskaitot jauno a)
   Sg = 0.5 + 0.5*S;
-  printf("%.2f\t%8.2f\t%8.2f\n", x, a, Sg); //print those
- }
+  //printf("%.2f\t%8.2f\t%8.2f\n", x, a, Sg); //So printf var aktivizet lai redzetu mainigo
+ }                                            //vertibas visa cikla garumaa
+  double a499 = a/((-1) *x*x) / (4*k*k - 2*k);
+  double s499 = 0.5 + 0.5*((Sg-0.5)/0.5)-(a/((-1) *x*x) / (4*k*k - 2*k)); //nevajadzigi gara iztekme
+  printf("a499 = %.2f S499 = %.2f\n", a499, s499);                        //bet pierada, ka s499 un S500
+  printf("a500 = %.2f S500 = %.2f\n", a, Sg);                             //ir praktiski identiski
   return Sg;
 }
 
 void main(){
- double x = 0.9, y, yy;
- y = cos(x/2)*cos(x/2);
+ double x, y, yy;
  //printf("standarta funkcija sin - y=sin(%.2f)=%.2f\n", x, y);
- printf("Funckijas cos(x/2)*cos(x/2) aprekinasana: \n");
+ printf("Funckijas cos(x/2)*cos(x/2) aprekinasana: \n"); //Funckija ir defineta pie visam x vertibam
  printf("Ludzu ievadit izveleto vertibu x: ");
- scanf(" %.2f", x);
+ scanf(" %lf", &x);
  y = cos(x/2)*cos(x/2);
- printf("cos(%.2f/2)*cos(%.2f/2)=%.2f\n", x, x, y);
-
- yy = mans_sinuss(x);
- printf("lietotaaja funkcija - y=mans_sinuss(%.2f)=%.2f\n", x, yy);
- asci();
-}void asci(){
+ printf("cos(%.2f/2)*cos(%.2f/2) = %.2f\n\n", x, x, y);
+ yy = teilors(x);
+ printf("Funckijas cos(%.2f/2)*cos(%.2f/2) vertiba izmantojot Teilora rindas saskaitisanu = %.2f\n", x, x, yy);
+ ascii();
+}void ascii(){
     printf("\n                                       500                      \n");
     printf("                                     _______                    \n");
     printf("                                    \\                          \n");
     printf("                                     \\          k   2*k        \n");
     printf("                      1     1         \\     (-1) * x           \n");
-    printf("   cos(x\2)*cos(x/2) = ___ + ___ *       > __________________   \n");
+    printf("   cos(x\2)*cos(x/2) = ___ + ___  *      > _______________      \n");
     printf("                                      /                         \n");
-    printf("                      2     2        /       (2*k)\!;           \n");
+    printf("                      2     2        /       (2*k)!             \n");
     printf("                                    /_______                    \n");
     printf("                                       k=0                      \n\n");
+    printf("                                       2                        \n");
+    printf("                               (-1) * x                         \n");
+    printf("   rekurences reizinatajs = _________________                   \n");
+    printf("                                                                \n");
+    printf("                                  2                             \n");
+    printf("                               4*k  - 2*k                       \n\n");
 }
